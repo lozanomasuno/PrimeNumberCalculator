07328e74b7ce56cbdc63e5a82c2655f4cf0f187c
lozanomasuno <lozanomasuno@gmail.com>
2020-07-13 15:14:52 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'primos';
+  public onlyNumbers: number;
+  public mayBePrimeNumber = 9879879879;
+  public isPrime = 'Is prime';
+  public isLoaded = false;
+  public isloaderLoaded = false;
+
+  public startLoading(): void {
+    this.isloaderLoaded = true;
+    this.isLoaded = false;
+    setTimeout(() => {
+      this.isLoaded = true;
+      this.isloaderLoaded = false;
+    }, 1000);
+  }
 }
